96ccfcd4bee52ec089f452c1a068b3436d68529e
nocis <nocis@tju.edu.cn>
2019-10-23 00:39:24 -0700

    add lab2

--- lab2/src/for_you_to_do.c
+++ lab2/src/for_you_to_do.c
@@ -75,10 +75,10 @@ void mydgemm(double *A, double *B, double *C, int n, int i, int j, int k, int b)
             for (k = 0; k < n; k += b)
             {
                 int i1 = 0;
-                for (i1 = i; i1 < i + b; i1 += 3)
+                for (i1 = i; i1 < (i + b > n? n : (i + b)); i1 += 3)
                 {
                     int j1 = 0;
-                    for (j1 = j; j1 < j + b; j1 += 3)
+                    for (j1 = j; j1 < (j + b > n? n : (j + b)); j1 += 3)
                     {
                         register double C_0_0 = C[i1 * n + j1];
                         register double C_1_0 = C[(i1 + 1) * n + j1];
@@ -93,7 +93,7 @@ void mydgemm(double *A, double *B, double *C, int n, int i, int j, int k, int b)
                         register double C_2_2 = C[(i1 + 2) * n + (j1 + 2)];
 
                         int k1 = 0;
-                        for (k1 = k; k1 < k + b; k1++)
+                        for (k1 = k; k1 < (k + b > n? n : (k + b)); k1++)
                         {
                             register double A_0_M = A[i1 * n + k1];
                             register double A_1_M = A[(i1 + 1) * n + k1];
