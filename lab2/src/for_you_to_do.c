e054ccf6c2924cfa0f88022d6510d22f4e3a5396
nocis <nocis@tju.edu.cn>
2019-10-26 16:21:54 -0700

    test

--- lab2/src/for_you_to_do.c
+++ lab2/src/for_you_to_do.c
@@ -531,6 +531,80 @@ int mydgetrf_non_squrare_v2(double* A, int pos, int* ipiv, int n, int bm, int bn
     free(tmpr);
     return 0;
 }
+
+
+
+
+
+void dgemm3_cache_mod(double *a, double *b, double *c, int n, int i, int j, int k, int blocksize)
+{
+    int i1 = i, j1 = j, k1 = k;
+    int ni = i + blocksize > n ? n : i + blocksize;
+    int nj = j + blocksize > n ? n : j + blocksize;
+    int nk = k + blocksize > n ? n : k + blocksize;
+
+    for (i1 = i; i1 < ni; i1 += 3)
+    {
+        for (j1 = j; j1 < nj; j1 += 3)
+        {
+            int t = i1 * n + j1;
+            int tt = t + n;
+            int ttt = tt + n;
+            register double c00 = c[t];
+            register double c01 = c[t + 1];
+            register double c02 = c[t + 2];
+            register double c10 = c[tt];
+            register double c11 = c[tt + 1];
+            register double c12 = c[tt + 2];
+            register double c20 = c[ttt];
+            register double c21 = c[ttt + 1];
+            register double c22 = c[ttt + 2];
+
+            for (k1 = k; k1 < nk; k1 += 3)
+            {
+		int l;
+                for (l = 0; l < 3; l++)
+                {
+                    int ta = i1 * n + k1 + l;
+                    int tta = ta + n;
+                    int ttta = tta + n;
+                    int tb = k1 * n + j1 + l * n;
+                    register double a0 = a[ta];
+                    register double a1 = a[tta];
+                    register double a2 = a[ttta];
+                    register double b0 = b[tb];
+                    register double b1 = b[tb + 1];
+                    register double b2 = b[tb + 2];
+
+                    c00 -= a0 * b0;
+                    c01 -= a0 * b1;
+                    c02 -= a0 * b2;
+                    c10 -= a1 * b0;
+                    c11 -= a1 * b1;
+                    c12 -= a1 * b2;
+                    c20 -= a2 * b0;
+                    c21 -= a2 * b1;
+                    c22 -= a2 * b2;
+                }
+            }
+            c[t] = c00;
+            c[t + 1] = c01;
+            c[t + 2] = c02;
+            c[tt] = c10;
+            c[tt + 1] = c11;
+            c[tt + 2] = c12;
+            c[ttt] = c20;
+            c[ttt + 1] = c21;
+            c[ttt + 2] = c22;
+        }
+    }
+}
+
+
+
+
+
+
 /**
  * 
  * this function computes triangular matrix - vector solver
