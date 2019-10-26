6299a07f383fb0ecb5f28ec3ef8ab2c1d8b79877
nocis <nocis@tju.edu.cn>
2019-10-26 16:13:26 -0700

    fix bugs
    debug
    
    
    debug
    
    
    debug
    
    
    debug
    
    
    debug
    
    
    debug
    
    
    debug
    
    
    debug
    
    
    debug
    
    
    debug
    
    
    debug
    
    
    debugs
    
    
    debug
    
    
    debug
    
    
    debug
    
    
    debug
    
    
    debug
    
    
    debug
    
    
    debug
    
    
    debug
    
    
    debug
    
    
    debug
    
    
    debug
    
    
    debug
    
    
    debug
    
    
    debug
    
    
    debug
    
    
    debug
    
    
    debug
    
    
    debug
    
    
    debug
    
    
    debug
    
    
    debug
    
    
    debug
    
    
    debug
    
    
    debug
    
    
    debug

--- lab2/src/for_you_to_do.c
+++ lab2/src/for_you_to_do.c
@@ -213,77 +213,75 @@ void mydgemm(const double* A, const double* B, double* C, const int m, const int
     }
 }
 
-void mydgemm_sub(double *A, double *B, double *C, int n, int i, int j, int k, int b)
+void mydgemm_sub(double *A, double *B, double *C, int m, int p, int n, int rowsize, int b)
 {
-    /* add your code here */
-    /* please just copy from your lab1 function optimal( ... ) */
-    /*int i = 0;
-    for (i = 0; i < n; i += b)
+int i = 0;
+    for (i = 0; i < m; i += b)
     {
         int j = 0;
         for (j = 0; j < n; j += b)
         {
             int k = 0;
-            for (k = 0; k < n; k += b)
+            for (k = 0; k < p; k += b)
             {
                 int i1 = 0;
-                for (i1 = i; i1 < (i + b > n? n : (i + b)); i1 += 3)
+                for (i1 = i; i1 < (i + b > m? m : (i + b)); i1 += 3)
                 {
                     int j1 = 0;
                     for (j1 = j; j1 < (j + b > n? n : (j + b)); j1 += 3)
                     {
-                        register double C_0_0 = C[i1 * n + j1];
-                        register double C_1_0 = C[(i1 + 1) * n + j1];
-                        register double C_2_0 = C[(i1 + 2) * n + j1];
+                        register double C_0_0 = C[i1 * rowsize + j1];
+                        register double C_1_0 = C[(i1 + 1) * rowsize + j1];
+                        register double C_2_0 = C[(i1 + 2) * rowsize + j1];
 
-                        register double C_0_1 = C[i1 * n + (j1 + 1)];
-                        register double C_1_1 = C[(i1 + 1) * n + (j1 + 1)];
-                        register double C_2_1 = C[(i1 + 2) * n + (j1 + 1)];
+                        register double C_0_1 = C[i1 * rowsize + (j1 + 1)];
+                        register double C_1_1 = C[(i1 + 1) * rowsize + (j1 + 1)];
+                        register double C_2_1 = C[(i1 + 2) * rowsize + (j1 + 1)];
 
-                        register double C_0_2 = C[i1 * n + (j1 + 2)];
-                        register double C_1_2 = C[(i1 + 1) * n + (j1 + 2)];
-                        register double C_2_2 = C[(i1 + 2) * n + (j1 + 2)];
+                        register double C_0_2 = C[i1 * rowsize + (j1 + 2)];
+                        register double C_1_2 = C[(i1 + 1) * rowsize + (j1 + 2)];
+                        register double C_2_2 = C[(i1 + 2) * rowsize + (j1 + 2)];
 
                         int k1 = 0;
-                        for (k1 = k; k1 < (k + b > n? n : (k + b)); k1++)
+                        for (k1 = k; k1 < (k + b > p? p : (k + b)); k1++)
                         {
-                            register double A_0_M = A[i1 * n + k1];
-                            register double A_1_M = A[(i1 + 1) * n + k1];
-                            register double A_2_M = A[(i1 + 2) * n + k1];
+                            register double A_0_M = A[i1 * p + k1];
+                            register double A_1_M = A[(i1 + 1) * p + k1];
+                            register double A_2_M = A[(i1 + 2) * p + k1];
 
                             register double B_M =  B[k1 * n + j1];
-                            C_0_0 += A_0_M * B_M;
-                            C_1_0 += A_1_M * B_M;
-                            C_2_0 += A_2_M * B_M;
+                            C_0_0 -= A_0_M * B_M;
+                            C_1_0 -= A_1_M * B_M;
+                            C_2_0 -= A_2_M * B_M;
 
                             B_M = B[k1 * n + (j1 + 1)];
-                            C_0_1 += A_0_M * B_M;
-                            C_1_1 += A_1_M * B_M;
-                            C_2_1 += A_2_M * B_M;
+                            C_0_1 -= A_0_M * B_M;
+                            C_1_1 -= A_1_M * B_M;
+                            C_2_1 -= A_2_M * B_M;
 
                             B_M = B[k1 * n + (j1 + 2)];
-                            C_0_2 += A_0_M * B_M;
-                            C_1_2 += A_1_M * B_M;
-                            C_2_2 += A_2_M * B_M;
+                            C_0_2 -= A_0_M * B_M;
+                            C_1_2 -= A_1_M * B_M;
+                            C_2_2 -= A_2_M * B_M;
 
                         }
-                        C[i1 * n + j1] = C_0_0;
-                        C[(i1 + 1) * n + j1] = C_1_0;
-                        C[(i1 + 2) * n + j1] = C_2_0;
+                        C[i1 * rowsize + j1] = C_0_0;
+                        C[(i1 + 1) * rowsize + j1] = C_1_0;
+                        C[(i1 + 2) * rowsize + j1] = C_2_0;
 
-                        C[i1 * n + (j1 + 1)] = C_0_1;
-                        C[(i1 + 1) * n + (j1 + 1)] = C_1_1;
-                        C[(i1 + 2) * n + (j1 + 1)] = C_2_1;
+                        C[i1 * rowsize + (j1 + 1)] = C_0_1;
+                        C[(i1 + 1) * rowsize + (j1 + 1)] = C_1_1;
+                        C[(i1 + 2) * rowsize + (j1 + 1)] = C_2_1;
 
-                        C[i1 * n + (j1 + 2)] = C_0_2;
-                        C[(i1 + 1) * n + (j1 + 2)] = C_1_2;
-                        C[(i1 + 2) * n + (j1 + 2)] = C_2_2;
+                        C[i1 * rowsize + (j1 + 2)] = C_0_2;
+                        C[(i1 + 1) * rowsize + (j1 + 2)] = C_1_2;
+                        C[(i1 + 2) * rowsize + (j1 + 2)] = C_2_2;
                     
                     }
                 }
             }
         }
-    }*/
+    }
     return;
 }
 
@@ -300,18 +298,19 @@ void mydgemm_sub(double *A, double *B, double *C, int n, int i, int j, int k, in
  * 
  **/
 
-void transpose(double* A, int n)
+void transpose(double* A, int m, int n)
 {
     int i, j;
+    double *tmp = (double*)malloc(sizeof(double) * n * m);
     for (i = 0; i < n; i++)
     {
-        for (j = 0; j < i; j++)
+        for (j = 0; j < m; j++)
         {
-            A[i * n + j] = A[i * n + j] ^ A[j * n + i];
-            A[j * n + i] = A[i * n + j] ^ A[j * n + i];
-            A[i * n + j] = A[i * n + j] ^ A[j * n + i];
+            tmp[i * n + j] = A[j * n + i];
         }
     }
+    memcpy(A, tmp, sizeof(double) * n * m);
+    free(tmp);
 }
 
 /**
@@ -334,7 +333,7 @@ void transpose(double* A, int n)
  *      return  0 : return normally 
  * 
  **/
-int mydgetrf_non_squrare(double* A, int pos, int* ipiv, int n, int bm, int bn)
+int mydgetrf_non_squrare(double* A, int pos, int* ipiv, int n, int bm, int bn, int b)
 {
     /* add your code here */
     int i, j, k;
@@ -343,10 +342,11 @@ int mydgetrf_non_squrare(double* A, int pos, int* ipiv, int n, int bm, int bn)
     double* LLT = (double*)malloc(sizeof(double) * bn * bn);
     double* AUR = (double*)malloc(sizeof(double) * bn * bn2);
     double* AURD = (double*)malloc(sizeof(double) * bn * bn2);
+    double* ALLD = (double*)malloc(sizeof(double) * bn2 * bn);
     double* LL = (double*)malloc(sizeof(double) * bn * bn);
     int* ipivl = (int*)malloc(sizeof(int) * bn);
 
-    for (i = 0; i < bm; i++)
+    for (i = 0; i < bn; i++)
     {
         int maxidx = i;
         double max = fabs(A[i * n + i]);
@@ -387,47 +387,57 @@ int mydgetrf_non_squrare(double* A, int pos, int* ipiv, int n, int bm, int bn)
         }
     }
 
-    memset(LLT, 0, bn * bn * sizeof(double));
-    memset(LL, 0, bn * bn * sizeof(double));
-    memset(ipivl, 0, bn * sizeof(int));
-    memset(AUR, 0, bn * (bn2) * sizeof(double));
-    memset(AURD, 0, bn * (bn2) * sizeof(double));
-
-    for (i = 0; i < bn; i++)
+    if (bn2 > 0)
     {
-        LLT[i * bn + i] = 1;
-    }
+        memset(LLT, 0, bn * bn * sizeof(double));
+        memset(LL, 0, bn * bn * sizeof(double));
+        memset(ipivl, 0, bn * sizeof(int));
+        memset(AUR, 0, bn * bn2 * sizeof(double));
+        memset(AURD, 0, bn * bn2 * sizeof(double));
+        memset(ALLD, 0, bn * bn2 * sizeof(double));
 
-    for (i = 0; i < bn; i++)
-    {
-        ipivl[i] = i;
-    }
-
-    for (i = 0; i < bn; i++)
-    {
-        LL[i * bn + i] = 1;
-        for (j = 0; j < i; j++)
+        for (i = 0; i < bn; i++)
         {
-            LL[i * bn + j] = A[i * n + j];
+            LLT[i * bn + i] = 1;
         }
-    }
 
-    for (i = 0; i < bn; i++)
-    {
-        memcpy(AUR + i * bn2, A + i * n + bn, bn2 * sizeof(double));
-    }
-    
-    for (i = 0; i < bn; i++)
-    {
-        mydtrsv('L', LL, LLT + i * bn, bn, ipivl);
-    }
+        for (i = 0; i < bn; i++)
+        {
+            ipivl[i] = i;
+        }
 
-    transpose(LLT, bn);
-    mydgemm(LLT, AUR, AURD, bn, bn, bn2, 126);
+        for (i = 0; i < bn; i++)
+        {
+            LL[i * bn + i] = 1;
+            for (j = 0; j < i; j++)
+            {
+                LL[i * bn + j] = A[i * n + j];
+            }
+        }
 
-    for (i = 0; i < bn; i++)
-    {
-        memcpy(A + i * n + bn, AURD + i * bn2, bn2 * sizeof(double));
+        for (i = 0; i < bn; i++)
+        {
+            memcpy(AUR + i * bn2, A + i * n + bn, bn2 * sizeof(double));
+        }
+
+        for (i = 0; i < bn; i++)
+        {
+            mydtrsv('L', LL, LLT + i * bn, bn, ipivl);
+        }
+
+        transpose(LLT, bn, bn);
+        mydgemm(LLT, AUR, AURD, bn, bn, bn2, b);
+
+        for (i = 0; i < bn; i++)
+        {
+            memcpy(A + i * n + bn, AURD + i * bn2, bn2 * sizeof(double));
+        }
+        for (i = 0; i < bn2; i++)
+        {
+            memcpy(ALLD + i * bn, A + i * n + bn * n, bn * sizeof(double));
+        }
+        mydgemm_sub(ALLD, AURD, A + bn * n + bn, bn2, bn, bn2, n, b);
+
     }
 
     free(LLT);
@@ -435,6 +445,89 @@ int mydgetrf_non_squrare(double* A, int pos, int* ipiv, int n, int bm, int bn)
     free(ipivl);
     free(AUR);
     free(AURD);
+    free(tmpr);
+    return 0;
+}
+
+int mydgetrf_non_squrare_v2(double* A, int pos, int* ipiv, int n, int bm, int bn, int b)
+{
+    /* add your code here */
+    int i, j, k;
+    int bn2 = bm - bn;
+    double* tmpr = (double*)malloc(sizeof(double) * n);
+    double* AUR = (double*)malloc(sizeof(double) * bn * bn2);
+    double* ALLD = (double*)malloc(sizeof(double) * bn2 * bn);
+
+    for (i = 0; i < bn; i++)
+    {
+        int maxidx = i;
+        double max = fabs(A[i * n + i]);
+        for (j = i + 1; j < bm; j++)
+        {
+            register double tmp = fabs(A[j * n + i]);
+            if (tmp - max > 1e-6)
+            {
+                maxidx = j;
+                max = tmp;
+            }
+        }
+
+        //too small pivot is also unacceptable
+        if (fabs(max - 0.0) < 1e-3)
+            return -1;
+
+        if (maxidx != i)
+        {
+            int newMaxidx = pos + maxidx;
+            int newI      = pos + i;
+            ipiv[newMaxidx] = ipiv[newMaxidx] ^ ipiv[newI];
+            ipiv[newI] = ipiv[newMaxidx] ^ ipiv[newI];
+            ipiv[newMaxidx] = ipiv[newMaxidx] ^ ipiv[newI];
+
+            swap(A-pos, tmpr, n, i, maxidx);
+        }
+
+        //do factorization
+        for (j = i + 1; j < bm; j++)
+        {
+            A[j * n + i] = A[j * n + i] / A[i * n + i];
+            register double A_j = A[j * n + i];
+            for (k = i + 1; k < bn; k++)
+            {
+                A[j * n + k] -= A_j * A[i * n + k];
+            }
+        }
+    }
+
+    if (bn2 > 0)
+    {
+        for (j = 0; j < (pos + b > n? n - pos : b); j++)
+        {
+            for (i = bn; i < n; i++)
+            {
+                register double A_J_I = A[j * n + i];
+                for (k = 0; k < j; k++)
+                {
+                    A_J_I -= A[j * n + k] * A[k * n + i];
+                }
+                A[j * n + i] = A_J_I;
+            }
+        }
+
+        for (i = 0; i < bn; i++)
+        {
+            memcpy(AUR + i * bn2, A + i * n + bn, bn2 * sizeof(double));
+        }
+
+        for (i = 0; i < bn2; i++)
+        {
+            memcpy(ALLD + i * bn, A + i * n + bn * n, bn * sizeof(double));
+        }
+        mydgemm_sub(ALLD, AUR, A + bn * n + bn, bn2, bn, bn2, n, b);
+    }
+
+    free(AUR);
+    free(ALLD);
     free(tmpr);
     return 0;
 }
@@ -472,11 +565,12 @@ int mydgetrf_block(double *A, int *ipiv, int n, int b)
     int i, j, k;
 
     double* Aptr = A;
-    for (i = 0; i < n; i += b)
+    for (i = 0; i < n - b; i += b)
     {
+        mydgetrf_non_squrare_v2(Aptr, i, ipiv, n, n - i, b, b);
         Aptr += b * n + b;
-        mydgetrf(Aptr, i, iptv, n, n - i, b);
     }
+    mydgetrf_non_squrare_v2(Aptr, (n / b) * b, ipiv, n, n % b, n % b, n % b);
     return 0;
 }
 
