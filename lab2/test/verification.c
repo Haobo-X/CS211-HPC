e61c9671d7273f0ad3205c45027a25af5d8d9369
nocis <nocis@tju.edu.cn>
2019-10-28 11:43:44 -0700

    debug

--- lab2/test/verification.c
+++ lab2/test/verification.c
@@ -80,7 +80,7 @@ int test_all_lu_functions()
             printf("my naive LU is incorrect.\n");
 
         t0 = get_sec();    
-        block_lu(A3, B3, n, 126);
+        block_lu_naive(A3, B3, n, 126);
         t1 = get_sec();
         printf("Elapsed time, block LU: %lf seconds\n", t1 - t0);
 
@@ -88,7 +88,7 @@ int test_all_lu_functions()
             printf("my block LU is incorrect.\n");
 
         t0 = get_sec();    
-        block_lu_naive(A4, B4, n, 126);
+        block_lu(A4, B4, n, 126);
         t1 = get_sec();
         printf("Elapsed time, block LU naive: %lf seconds\n", t1 - t0);
 
