0581bfabb556583ac49933adf7ffed9f243cd2de
nocis <nocis@tju.edu.cn>
2019-11-20 12:35:31 -0800

    fix bugs

--- lab3/sieve1.c
+++ lab3/sieve1.c
@@ -103,6 +103,9 @@ int main (int argc, char *argv[])
       MPI_Reduce(&count, &global_count, 1, MPI_INT, MPI_SUM,
                   0, MPI_COMM_WORLD);
 
+   global_count++;
+   //add 2 
+
    /* Stop the timer */
    elapsed_time += MPI_Wtime();
 
