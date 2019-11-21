a7c6b4fa05dfea5eb73643690d9ef1263fa4fe63
nocis <nocis@tju.edu.cn>
2019-11-21 11:47:19 -0800

    test

--- lab3/sieve1.c
+++ lab3/sieve1.c
@@ -94,6 +94,8 @@ int main (int argc, char *argv[])
          prime = 2 * index + 3;
       }
       if (p > 1) MPI_Bcast(&prime, 1, MPI_INT, 0, MPI_COMM_WORLD);
+      if (prime * prime > high_value)
+         break;
    } while (prime * prime <= high_value);
    count = 0;
    for (i = 0; i < size; i++)
